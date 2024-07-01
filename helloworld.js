66e53b6b34a62a6d0a4c285dc8227cd6583022e1
adiam16 <bezibaba121621@gmail.com>
2024-07-01 12:28:52 +0300

    Add files via upload
    HELLO everyone here I came up with my first Java script code if you have any comments or suggestions please tell me to improve it. THANK YOU!

---
+++ helloworld.js
@@ -0,0 +1,12 @@
+// This is my first java script code!
+let greeting = ('Hello world!');
+ console.log(greeting);
+let name ='Bezawit Beyene';
+ console.log( name );
+ let learningInstitution =('ADISS ABEBA UNIVERSTY');
+ console.log(learningInstitution);
+ let department = ('pre-enginnering');
+ console.log(department);
+ let idNumber =('UGR/8349/16');
+ console.log(idNumber)
+ // Hello every one here it is my first java script code as i mentioned and if you have any comment or suggestion in my work you can tell me this comment section also and thankyou for your seeing!s
